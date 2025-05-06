9a40cccddc972486c38e70d098d7904a130671bc
sumit316-glitch <sy09896@gmail.com>
2025-05-06 13:26:48 +0530

    component deleted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ import headerImg from '../assets/header.png';
 export default function Hero() {
   return (
     <section className="hero">
-<img src={headerImg} alt="Featured House" />
-<div className="hero-content">
+    <img src={headerImg} alt="Featured House" />
+    <div className="hero-content">
         <h1>
           For Sale<br />
           10A CAROLINE STREET,<br />
